93a931755cdf57199758e3e21b26557ed0345469
tatsuo.sakurai <tatsuo.sakurai@gmail.com>
2013-05-26 16:45:16 +0900

    Add table view to AlphabetController

--- app/controllers/AlphabetController.rb
+++ app/controllers/AlphabetController.rb
@@ -6,5 +6,22 @@ class AlphabetController < UIViewController
 
     @table = UITableView.alloc.initWithFrame(self.view.bounds)
     self.view.addSubview @table
+    @table.dataSource = self
+    @data = ("A".."Z").to_a
+  end
+
+  def tableView(tableView, cellForRowAtIndexPath: indexPath)
+    @reuseIdentifier ||= "CELL_INDENTIFIER"
+
+    cell = tableView.dequeueReusableCellWithIdentifier(@reuseIndentifier) || begin
+      UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier:@reuseIdentifier)
+    end
+    cell.textLabel.text = @data[indexPath.row]
+
+    cell
+  end
+
+  def tableView(tableView, numberOfRowsInSection: section)
+    @data.count
   end
 end
